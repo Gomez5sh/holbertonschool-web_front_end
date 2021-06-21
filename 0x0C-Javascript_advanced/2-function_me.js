ae4a8ca8d7d668b9e70fbcc318966e34ab5cce64
gomez5sh <emmanuel.gomez@vertebrasoluciones.com>
2021-06-21 10:08:54 -0500

    [ADD]: Add task 0 to 2

---
+++ 0x0C-Javascript_advanced/2-function_me.js
@@ -0,0 +1,7 @@
+function welcomeMessage(fullName) {
+    alert(`Welcome ${fullName}`);
+}
+
+let guillaume = welcomeMessage("Guillaume");
+let alex = welcomeMessage("alex");
+let fred = welcomeMessage("fred");
